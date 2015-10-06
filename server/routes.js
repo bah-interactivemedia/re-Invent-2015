d507bc98ba40c0be135159a62d097de09951000c
Tony Chen <tonychen091@gmail.com>
2015-10-06 13:01:05 -0700

    standing up dummy link

--- server/routes.js
+++ server/routes.js
@@ -11,14 +11,20 @@ module.exports = function(app) {
 
   // Insert routes below
   app.use('/api/things', require('./api/thing'));
-  
+
   // All undefined asset or api routes should return a 404
   app.route('/:url(api|auth|components|app|bower_components|assets)/*')
    .get(errors[404]);
 
   // All other routes should redirect to the index.html
-  app.route('/*')
+  app.route('/')
     .get(function(req, res) {
       res.sendFile(path.resolve(app.get('appPath') + '/index.html'));
     });
+
+  app.route('/testdata')
+    .get(function(req, res){
+      res.setHeader('Content-Type', 'application/json');
+      res.send(JSON.stringify({ a: 1 }));
+    })
 };
