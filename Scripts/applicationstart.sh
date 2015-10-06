ac4fc6d87dc2620f6b2502958d47ef7ade7d04be
unknown <ross.serino@IPRW-R907GUBA.inforeliance.com>
2015-10-06 17:45:05 -0400

    updated application start

--- Scripts/applicationstart.sh
+++ Scripts/applicationstart.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 . ~/.bashrc
 
-cd /var/www/donorschoose/Source/
+cd /var/www/donorschoose/
 npm update
 npm install
 bower install
 grunt build
-cd /var/www/donorschoose/Source/dist/server
+cd /var/www/donorschoose/server
 export NODE_ENV=test
-nodejs app.js >> /home/ubuntu/gruntlog/nodelog.log &
+nodejs app.js >> /home/ec2-user/nodelog.log &
 exit 0
