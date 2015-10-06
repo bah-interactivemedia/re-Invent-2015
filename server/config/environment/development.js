5455933bb27359d477311d72e79ef37a4f982043
Michael Bray <bray_michael@bah.com>
2015-10-06 10:24:31 -0700

    Yo setup with angular-fullstack. You can run with "grunt" --> "grunt serve". You'll need to download MongoDB (https://docs.mongodb.org/manual/tutorial/install-mongodb-on-os-x/) and have the mongod process running before "grunt serve".

---
+++ server/config/environment/development.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://localhost/reinvent2015-dev'
+  },
+
+  seedDB: true
+};
---
+++ server/config/environment/test.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// Test specific configuration
+// ===========================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://localhost/reinvent2015-test'
+  }
+};
