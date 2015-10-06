cb9110f01703b961017d336a53386f687bf7432c
unknown <ross.serino@IPRW-R907GUBA.inforeliance.com>
2015-10-06 19:13:44 -0400

    added additional messages to nodejs startup

--- Scripts/applicationstart.sh
+++ Scripts/applicationstart.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 . ~/.bashrc
 
+export NODE_ENV=production
+
 cd /var/www/donorschoose/
 npm update
 npm install
 bower install
+
 grunt build --force
+
 cd /var/www/donorschoose/dist/server
-export NODE_ENV=production
+
+echo "Starting NodeJS"
 node app.js >> /home/ec2-user/nodelog.log &
+echo "NodeJS started"
+
 exit 0
